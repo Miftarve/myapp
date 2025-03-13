e65d22215d8a958e4d07f9ed6ba7fde21eee5a9a
veliqi.miftar.studente <veliqi.miftar.studente@itispaleocapa.it>
2025-03-13 16:54:58 +0000

    Footballmify

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Football App',
+      title: 'Footballmify App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
